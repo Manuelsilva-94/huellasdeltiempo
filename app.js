5a1e44a0eb5cc708fcff1cbba3410d1eaf5a7e53
manuel silva <manuelsilvamdo@gmail.com>
2020-07-10 01:17:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var burgerContainer = document.querySelector(".burger-container"),
+    nav = document.querySelector("nav"),
+    navOpen = false,
+    header = document.querySelector("header");
+
+burgerContainer.addEventListener("click", () => {
+    if (!navOpen) {
+        nav.style.right = "0";
+        header.style.overflow = "visible";
+        burgerContainer.classList.add("burger-open");
+        navOpen = true;
+    } else {
+        nav.style.right = "-60%";
+        header.style.overflow = "hidden";
+        burgerContainer.classList.remove("burger-open");
+        navOpen = false;
+    }
+});
